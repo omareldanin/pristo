9ad24d84013043f5a2899058e0d554f9fc5b91f6
omar mohamed <omer.eldanin@gmail.com>
2025-05-28 02:02:03 +0300

    add products

--- src/app/home-category/home-category.response.ts
+++ src/app/home-category/home-category.response.ts
@@ -42,6 +42,7 @@ export const categoryReform = (
 
   const now = dayjs(); // current time
   const dayKey = now.format('dddd').toLowerCase(); // e.g. "monday"
+  now.format('hh:mm A');
   console.log(now);
 
   const categoryReformed = {
